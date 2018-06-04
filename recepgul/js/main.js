a8124d1d4563cdf130fa40cd7fb3357cf4f25e07
Recep Gül <37611104+recep6878@users.noreply.github.com>
2018-06-04 22:40:09 +0300

    Add files via upload

---
+++ recepgul/js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+
+    $('.menu-acilir-btn').click(function() {
+
+        var acilanmenu = $('.menu-acilir');
+
+        if(acilanmenu.css('display') == 'none') {
+
+        }
+
+        acilanmenu.slideToggle();
+
+    });
+
+});
